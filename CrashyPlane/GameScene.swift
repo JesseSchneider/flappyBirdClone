871e9b5a68f7c72ae7639726b16f89f87094e15c
JesseSchneider <jmschneider430@gmail.com>
2015-09-18 16:19:46 -0400

    added physics, music, scoring

--- CrashyPlane/GameScene.swift
+++ CrashyPlane/GameScene.swift
@@ -9,39 +9,105 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+enum GameState {
+    case ShowingLogo
+    case Playing
+    case Dead
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var player: SKSpriteNode!
-    
     var scoreLabel: SKLabelNode!
-    
+    var backgroundMusic: SKAudioNode!
     var score = 0 {
         didSet {
             scoreLabel.text = "SCORE: \(score)"
         }
     }
+    var logo: SKSpriteNode!
+    var gameOver: SKSpriteNode!
+    var gameState = GameState.ShowingLogo
     
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
+        /* Setup scene here */
         
         createPlayer()
         createSky()
         createBackground()
         createGround()
-        initRocks()
         createScore()
+        createLogos()
+        
+        physicsWorld.gravity = CGVectorMake(0.0, -5.0)
+        physicsWorld.contactDelegate = self
+        
+        backgroundMusic = SKAudioNode(fileNamed: "music.m4a")
+        addChild(backgroundMusic)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         /* Called when a touch begins */
-        
-        
+        switch gameState {
+        case .ShowingLogo:
+            gameState = .Playing
+            
+            let fadeOut = SKAction.fadeOutWithDuration(0.5)
+            let remove = SKAction.removeFromParent()
+            let wait = SKAction.waitForDuration(0.5)
+            let activatePlayer = SKAction.runBlock { [unowned self] in
+                self.player.physicsBody?.dynamic = true
+                self.initRocks()
+            }
+            
+            let sequence = SKAction.sequence([fadeOut, wait, activatePlayer, remove])
+            logo.runAction(sequence)
+            
+        case .Playing:
+            player.physicsBody?.velocity = CGVectorMake(0, 0)
+            player.physicsBody?.applyImpulse(CGVectorMake(0, 20))
+            
+        case .Dead:
+            break
+        }
     }
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        let value = player.physicsBody!.velocity.dy * 0.001
+        let rotate = SKAction.rotateToAngle(value, duration: 0.1)
         
+        player.runAction(rotate)
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        if contact.bodyA.node?.name == "scoreDetect" || contact.bodyB.node?.name == "scoreDetect" {
+            if contact.bodyA.node == player {
+                contact.bodyB.node?.removeFromParent()
+            } else {
+                contact.bodyA.node?.removeFromParent()
+            }
+            
+            let sound = SKAction.playSoundFileNamed("coin.wav", waitForCompletion: false)
+            runAction(sound)
+            
+            ++score
+            
+            return
+        }
         
+        if contact.bodyA.node == player || contact.bodyB.node == player {
+            if let explosion = SKEmitterNode(fileNamed: "PlayerExplosion.sks") {
+                explosion.position = player.position
+                addChild(explosion)
+            }
+            
+            let sound = SKAction.playSoundFileNamed("explosion.wav", waitForCompletion: false)
+            runAction(sound)
+            
+            player.removeFromParent()
+            speed = 0
+        }
     }
     
     func createPlayer() {
@@ -51,6 +117,11 @@ class GameScene: SKScene {
         player.position = CGPoint(x: frame.width / 6, y: frame.height * 0.75)
         
         addChild(player)
+        
+        player.physicsBody = SKPhysicsBody(texture: playerTexture, size: playerTexture.size())
+        player.physicsBody!.contactTestBitMask = player.physicsBody!.collisionBitMask
+        player.physicsBody?.dynamic = false
+        player.physicsBody?.collisionBitMask = 0
         
         let frame2 = SKTexture(imageNamed: "player-2")
         let frame3 = SKTexture(imageNamed: "player-3")
@@ -105,6 +176,9 @@ class GameScene: SKScene {
             ground.zPosition = -10
             ground.position = CGPoint(x: (groundTexture.size().width / 2.0 + (groundTexture.size().width * CGFloat(i))), y: groundTexture.size().height / 2)
             
+            ground.physicsBody = SKPhysicsBody(texture: ground.texture!, size: ground.texture!.size())
+            ground.physicsBody?.dynamic = false
+            
             addChild(ground)
             
             let moveLeft = SKAction.moveByX(-groundTexture.size().width, y: 0, duration: 5)
@@ -121,10 +195,17 @@ class GameScene: SKScene {
         let rockTexture = SKTexture(imageNamed: "rock")
         
         let topRock = SKSpriteNode(texture: rockTexture)
+        
+        topRock.physicsBody = SKPhysicsBody(texture: rockTexture, size: rockTexture.size())
+        topRock.physicsBody?.dynamic = false
+        
         topRock.zRotation = CGFloat(M_PI)
         topRock.xScale = -1.0
         
         let bottomRock = SKSpriteNode(texture: rockTexture)
+        
+        bottomRock.physicsBody = SKPhysicsBody(texture: rockTexture, size: rockTexture.size())
+        bottomRock.physicsBody?.dynamic = false
         
         topRock.zPosition = -20
         bottomRock.zPosition = -20
@@ -132,6 +213,10 @@ class GameScene: SKScene {
         
         // 2. Create a third sprite that is a large red rectangle. This will be positioned just after the rocks and will be used to track when the player has passed through the rocks safely – if they touch that red rectangle, they should score a point. (Don't worry, we'll make it invisible later!)
         let rockCollision = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: 32, height: frame.height))
+        
+        rockCollision.physicsBody = SKPhysicsBody(rectangleOfSize: rockCollision.size)
+        rockCollision.physicsBody?.dynamic = false
+        
         rockCollision.name = "scoreDetect"
         
         addChild(topRock)
@@ -144,31 +229,24 @@ class GameScene: SKScene {
         
         let max = Int(frame.height / 3)
         
-        if #available(iOS 9.0, *) {
-            let rand = GKRandomDistribution(lowestValue: -100, highestValue: max)
-            let yPosition = CGFloat(rand.nextInt())
-            // this next value affects the width of the gap between rocks
-            // make it smaller to make your game harder – if you're feeling evil!
-            let rockDistance: CGFloat = 70
-            
-            // 4. Position the rocks just off the right edge of the screen, then animate them across to the left edge. When they are safely off the left edge, remove them from the game.
-            topRock.position = CGPoint(x: xPosition, y: yPosition + topRock.size.height + rockDistance)
-            bottomRock.position = CGPoint(x: xPosition, y: yPosition - rockDistance)
-            rockCollision.position = CGPoint(x: xPosition + (rockCollision.size.width * 2), y: CGRectGetMidY(frame))
-            
-            let endPosition = frame.width + (topRock.frame.width * 2)
-            
-            let moveAction = SKAction.moveByX(-endPosition, y: 0, duration: 5.8)
-            let moveSequence = SKAction.sequence([moveAction, SKAction.removeFromParent()])
-            topRock.runAction(moveSequence)
-            bottomRock.runAction(moveSequence)
-            rockCollision.runAction(moveSequence)
-            
-        }
+        let rand = GKRandomDistribution(lowestValue: -100, highestValue: max)
+        let yPosition = CGFloat(rand.nextInt())
+        // this next value affects the width of the gap between rocks
+        // make it smaller to make your game harder – if you're feeling evil!
+        let rockDistance: CGFloat = 70
         
-        else {
-            // TODO: Handle for iOS 8
-        }
+        // 4. Position the rocks just off the right edge of the screen, then animate them across to the left edge. When they are safely off the left edge, remove them from the game.
+        topRock.position = CGPoint(x: xPosition, y: yPosition + topRock.size.height + rockDistance)
+        bottomRock.position = CGPoint(x: xPosition, y: yPosition - rockDistance)
+        rockCollision.position = CGPoint(x: xPosition + (rockCollision.size.width * 2), y: CGRectGetMidY(frame))
+        
+        let endPosition = frame.width + (topRock.frame.width * 2)
+        
+        let moveAction = SKAction.moveByX(-endPosition, y: 0, duration: 5.8)
+        let moveSequence = SKAction.sequence([moveAction, SKAction.removeFromParent()])
+        topRock.runAction(moveSequence)
+        bottomRock.runAction(moveSequence)
+        rockCollision.runAction(moveSequence)
     }
     
     func initRocks() {
@@ -194,6 +272,17 @@ class GameScene: SKScene {
         addChild(scoreLabel)
     }
 
+    func createLogos() {
+        logo = SKSpriteNode(imageNamed: "logo")
+        logo.position = CGPoint(x: CGRectGetMidX(frame), y: CGRectGetMidY(frame))
+        addChild(logo)
+        
+        gameOver = SKSpriteNode(imageNamed: "gameover")
+        gameOver.position = CGPoint(x: CGRectGetMidX(frame), y: CGRectGetMidY(frame))
+        gameOver.alpha = 0
+        addChild(gameOver)
+    }
+    
 }
 
 
